8e91ea7533499972b39e44fa9db785330135ef01
orange634nty <orange634nty@gmail.com>
2019-05-03 16:45:03 +0900

    initial commit

---
+++ Assets/DragMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, ICanvasRaycastFilter
+{
+    private bool isDragging = false;
+
+    /// <summary>
+    /// IBeginDragHandlerのインターフェイス
+    /// ドラック状態をonにする
+    /// </summary>
+    /// <param name="data">ドラッグ開始時に呼び出されるPointerEventData</param>
+    public void OnBeginDrag(PointerEventData data)
+    {
+        isDragging = true;
+    }
+
+    /// <summary>
+    /// IDragHandlerのインターフェイス
+    /// PointerEventDataの移動差分をDragPanelのtransformに反映する
+    /// </summary>
+    /// <param name="data">ドラッグ中に呼び出されるPointerEventData</param>
+    public void OnDrag(PointerEventData data)
+    {
+        transform.position += new Vector3(data.delta.x, data.delta.y);
+    }
+
+    /// <summary>
+    /// IEndDragHandlerのインターフェイス
+    /// ドラッグ状態をoffにする
+    /// </summary>
+    /// <param name="data">ドラッグが終了したときに呼び出されるPointerEventData</param>
+    public void OnEndDrag(PointerEventData data)
+    {
+        isDragging = false;
+    }
+
+    /// <summary>
+    /// ICanvasRaycastFilterのインターフェイス
+    /// 移動中はRaycastを効かなくすることで、他のイベントを起こさせないようにする
+    /// </summary>
+    /// <param name="sp"></param>
+    /// <param name="cam"></param>
+    /// <returns></returns>
+    public bool IsRaycastLocationValid(Vector2 sp, Camera cam)
+    {
+        return !isDragging;
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isDroped;
+
+    /// <summary>
+    /// ドラッグ開始したときに呼び出して、パネルの色を変える
+    /// DragPanelのEventTriggerコンポーネントの
+    /// BeginDragイベントで呼び出すように設定
+    /// </summary>
+    /// <param name="data">PointerEventData</param>
+    public void OnBeginDrag(BaseEventData data)
+    {
+        Image image = ((PointerEventData)data).pointerDrag.GetComponent<Image>();
+        // ドラッグ中は半透明マゼンダ色
+        image.color = new Color(1.0f, 0.0f, 1.0f, 0.5f);
+        isDroped = false;
+    }
+
+    /// <summary>
+    /// ドラッグした後の判定
+    /// SafeAreaのEventTriggerコンポーネントの
+    /// OnDropイベント時に呼び出すように設定
+    /// </summary>
+    /// <param name="data">PointerEventData</param>
+    public void OnDrop(BaseEventData data)
+    {
+        isDroped = true;
+    }
+
+    /// <summary>
+    /// ドラッグ終了したときに呼び出して状況に応じてパネルの色を変更
+    /// DragPanelのEventTriggerコンポーネントの
+    /// EndDragイベントで呼び出すように設定
+    /// </summary>
+    /// <param name="data">PointerEventData</param>
+    public void OnEndDrag(BaseEventData data)
+    {
+        Image image = ((PointerEventData)data).pointerDrag.GetComponent<Image>();
+        if (isDroped)
+        {
+            // 緑ゾーンに降りたらマゼンダ色になる
+            image.color = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+        }
+        else
+        {
+            // 通常状態
+            image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        isDroped = false;
+    }
+}
